cc9d3a9d269bcdbc4cd29112c433b182677d731a
BariscanTosyali <bariscantosyali@gmail.com>
2026-02-11 14:17:55 +0300

    Fix full_name function and pass all tests

--- hello.py
+++ hello.py
@@ -3,18 +3,20 @@
 import sys
 
 def full_name(first_name, last_name):
-    """returns the full name"""
-    name = f"{first_name.capitalize()}{last_name.capitalize()}"
-    
+    """returns the full name without extra spaces"""
+    # capitalize() metoduyla isimleri düzenliyoruz
+    # .strip() ile eğer isimlerden biri boşsa oluşacak kenar boşluklarını temizliyoruz
+    name = f"{first_name.capitalize()} {last_name.capitalize()}".strip()
+
     return name
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
-        # => ['hello.py']
-        print(f'Hello{full_name("", "")}!')
+        # Hiç argüman girilmezse: "Hello !"
+        print(f'Hello {full_name("", "")}!')
     elif len(sys.argv) == 2:
-        # => ['hello.py', 'John' ]
+        # Sadece ilk isim girilirse: "Hello John!"
         print(f'Hello {full_name(sys.argv[1], "")}!')
     else:
-        # => ['hello.py', 'John', 'Lennon']
+        # İki isim de girilirse: "Hello John Lennon!"
         print(f"Hello {full_name(sys.argv[1], sys.argv[2])}!")
